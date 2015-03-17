a5df5607a519d7ae131c0777360ac9e08d92c2ec
Duncan <dac80@zips.uakron.edu>
2015-03-17 02:24:08 -0400

    Added the initial version of Luna

---
+++ src/luna/SpriteSheet.h
@@ -0,0 +1,11 @@
+//Luna Sprite Sheet Header File
+//Created by Duncan Campbell
+
+#ifndef LUNA_SPRITE_SHEET_H
+#define LUNA_SPRITE_SHEET_H
+
+class SpriteSheet {
+		
+}
+
+#endif
---
+++ src/luna/TileSheet.h
@@ -0,0 +1,17 @@
+//Tile Sheet Header File
+//Created by Duncan Campbell
+
+#ifndef TILE_SHEET_H
+#define TILE_SHEET_H
+
+namespace Luna {
+
+	class TileSheet {
+		public:
+		
+		private:	
+	};
+	
+}
+
+#endif
