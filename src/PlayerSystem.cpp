066ea2c7fa9722ea2f4a958ba7b6e5c37254a9aa
Duncan Campbell <dac80@zips.uakron.edu>
2015-04-21 19:28:24 -0400

    Finished adding the parts to make the Input Manger work with the player.

--- src/PlayerSystem.cpp
+++ src/PlayerSystem.cpp
@@ -19,14 +19,17 @@ void PlayerSystem::updateSystem() {
     int cameraY = Engine::camera->getY();
 
     switch(Engine::world->getInputManager()->getKeyPressed()) {
-      case Luna::UP_ARROW   : cameraY -= 1;
+      case Luna::UP_ARROW   : cameraY += 1;
                                   break;
-      case Luna::DOWN_ARROW : cameraY += 1;
+      case Luna::DOWN_ARROW : cameraY -= 1;
                                   break;
-      case Luna::RIGHT_ARROW: cameraX += 1;
+      case Luna::RIGHT_ARROW: cameraX -= 1;
                                   break;
-      case Luna::LEFT_ARROW : cameraX -= 1;
+      case Luna::LEFT_ARROW : cameraX += 1;
                                   break;
     }
+
+    Engine::camera->setX(cameraX);
+	 Engine::camera->setY(cameraY);
   }
 }
